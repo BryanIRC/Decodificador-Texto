77b2048525d0b4a6927931dcc0fc9b597c8d8595
BryanIRC <brios.16@gmail.com>
2023-05-30 23:37:26 -0500

    Funcion copiar

--- scrip.js
+++ scrip.js
@@ -2,15 +2,20 @@ const txt_noencrypt = document.querySelector(".txt-noencrypt");
 const txt_encrypt = document.querySelector(".txt-encrypt");
 
 function BtnEncrypt() {
-    let textencrypt = EncryptTxt(txt_noencrypt.value);
-    txt_encrypt.value = textencrypt;
+    let textvalidated=false;
+    textvalidated = ValidateTxt(txt_noencrypt.value);
+    if((textvalidated)){
+        alert("El texto que intenta encriptar contiene acentos, caracteres especiales y/o números");
+    }
+    else{
+        let textencrypt = EncryptTxt(txt_noencrypt.value);
+        txt_encrypt.value = textencrypt;
+    }
 }
-
 function BtnUnscrypt() {
     let textencrypt = UnscryptTxt(txt_noencrypt.value);
     txt_encrypt.value = textencrypt;
 }
-
 function EncryptTxt(txtencrypt) {
     let keys = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
     txtencrypt = txtencrypt.toLowerCase();
@@ -21,14 +26,33 @@ function EncryptTxt(txtencrypt) {
     }
     return txtencrypt;
 }
-
-function UnscryptTxt(txtencrypt) {
+function UnscryptTxt(txtunscrypt) {
     let keys = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
-    txtencrypt = txtencrypt.toLowerCase();
     for (let i = 0; i < keys.length; i++) {
-        if (txtencrypt.includes(keys[i][1])) {
-            txtencrypt = txtencrypt.replaceAll(keys[i][1], keys[i][0]);
+        if (txtunscrypt.includes(keys[i][1])) {
+            txtunscrypt = txtunscrypt.replaceAll(keys[i][1], keys[i][0]);
         }
     }
-    return txtencrypt;
+    return txtunscrypt;
+}
+function ValidateTxt(txtvalidated) {
+    for (let i = 33; i <=64; i++) {
+        if (txtvalidated.includes(String.fromCharCode(i))) {
+            return true;
+        }
+    }
+    for (let i = 91; i <=96; i++) {
+        if (txtvalidated.includes(String.fromCharCode(i))) {
+            return true;
+        }
+    }
+    for (let i = 123; i <=255; i++){
+        if(i<164 && i>165){
+            if (txtvalidated.includes(String.fromCharCode(i))) {
+                return true;}
+            }}
+    return false;
+}
+function CopyTxt(){
+    navigator.clipboard.writeText(txt_encrypt.value);
 }
